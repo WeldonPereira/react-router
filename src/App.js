b77dd26d5f0e06db521f2811da370004facae2f3
Weldon <weldonbarros179@gmail.com>
2025-06-13 08:47:25 -0300

    feat: adicionar NotFound

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red", fontSize: "2.5rem"}}>A página não foi encontrada!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
